dbcbe7e473bcc1d15a3984b3ca35018127d1bc76
Charles Ferreira <charles.dev@live.com>
2019-03-07 14:41:41 -0300

    Resolve #2

--- db.py
+++ db.py
@@ -1,5 +1,13 @@
+# Database code for the Log Analysis
+
+import psycopg2
+
+DBNAME = 'news'
+
 def get_popular_articles():
-    return [{
-        'title': 'Dummy text',
-        'views': 123
-    }]
+    db = psycopg2.connect(database=DBNAME)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM article_views ORDER BY views DESC LIMIT 3")
+    articles = cursor.fetchall()
+    db.close()
+    return articles
