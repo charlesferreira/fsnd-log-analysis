a07faff6126f9377ecda2f971f9d72a93aaa3148
Charles Ferreira <charles.dev@live.com>
2019-03-07 10:59:04 -0300

    Create separate folder for HTML templates

--- report.py
+++ report.py
@@ -2,32 +2,15 @@
 #
 # A simple reporting tool for the Full Stack Web Developer Nanodegree @ Udacity
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# HTML template for the starting page
-HTML = '''\
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Log analysis</title>
-  </head>
-  <body>
-    <h1>Log analysis</h1>
-    <ul>
-        <li><a href="#">Most popular articles</a></li>
-        <li><a href="#">Most popular authors</a></li>
-        <li><a href="#">Most error requests</a></li>
-    </ul>    
-  </body>
-</html>
-'''
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def main():
     ''' Starting page '''
-    return HTML
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
